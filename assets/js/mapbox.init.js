a6570d93d6e5b90916d537aa9deb7465936a8577
BONAFIDE BOSS <bonafideboss@gmail.com>
2021-11-04 12:01:42 -0400

    Initial commit

---
+++ assets/js/mapbox.init.js
@@ -0,0 +1,27 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ0dGVtcGxhdGUiLCJhIjoiY2s0M3I5ZHgzMGEzNDNucXM1cDd0dzl3cSJ9.a2wjLlxz8LzWj9nIoGsshw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [51.594088, 25.162102], // starting position
+    zoom: 13 // starting zoom
+});
+
+// create the popup
+var popup = new mapboxgl.Popup({
+    offset: 40
+}).setText(
+    'Al Wakrah, '
+);
+
+// create DOM element for the marker
+var el = document.createElement('div');
+el.id = 'marker';
+
+// create the marker
+new mapboxgl.Marker(el)
+    .setLngLat([51.594088, 25.162102])
+    .setPopup(popup) // sets a popup on this marker
+    .addTo(map);
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
