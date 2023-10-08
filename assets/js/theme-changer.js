676d569a4baf9a326c789728e41dbb2f8fd91722
BONAFIDE BOSS <amaanalmir@gmail.com>
2023-10-08 09:46:33 +0300

    update

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -1,5 +1,7 @@
 const default_theme = "dark"
 const secondary_theme = "light"
+const default_theme_icon = `<i class="fa-duotone fa-moon text-white"></i>`
+const secondary_theme_icon = `<i class="fa-duotone fa-sun-bright text-warning"></i>`
 
 function get_theme() {
   const theme = localStorage.getItem('theme')
@@ -12,15 +14,10 @@ function set_theme(theme) {
 
 function apply_theme(theme = null) {
   theme = theme ? theme : get_theme()
-
-  const icon_dark = `<i class="fa-duotone fa-moon text-white"></i>`
-  const icon_light = `<i class="fa-duotone fa-sun-bright text-warning"></i>`
-  const theme_icon = (theme == "dark") ? icon_dark : icon_light
-
+  const theme_icon = (theme == default_theme) ? default_theme_icon : secondary_theme_icon
   $("html").attr("data-bs-theme", theme)
   $('#theme').html(theme_icon)
 }
-
 apply_theme()
 
 $("#theme").on("click", function () {
@@ -28,5 +25,4 @@ $("#theme").on("click", function () {
   theme = (theme == default_theme) ? secondary_theme : default_theme
   set_theme(theme)
   apply_theme(theme)
-
 });
